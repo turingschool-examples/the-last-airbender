126e9863c0d12bfd9299f0ff26066d0629e1b1f6
Alexa Morales Smyth <alexamsmyth@gmail.com>
2021-04-05 12:53:43 -0400

    Refactor tests to expect openstruct objects

--- spec/facades/search_facade_spec.rb
+++ spec/facades/search_facade_spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
+require 'ostruct'
 
 RSpec.describe SearchFacade do
   describe "Happy path" do
@@ -6,7 +7,7 @@ RSpec.describe SearchFacade do
 
       expect(SearchFacade.get_nation_member_data("fire_nation").class).to eq(Array)
       expect(SearchFacade.get_nation_member_data("fire_nation").count).to eq(97)
-      expect(SearchFacade.get_nation_member_data("fire_nation").first.class).to eq(Hash)
+      expect(SearchFacade.get_nation_member_data("fire_nation").first.class).to eq(OpenStruct)
     end
   end
   describe "Sad path" do
@@ -14,6 +15,19 @@ RSpec.describe SearchFacade do
 
       expect(SearchFacade.get_nation_member_data("ohio_state").class).to eq(Array)
       expect(SearchFacade.get_nation_member_data("ohio_state").count).to eq(0)
+    end
+  end
+
+  describe "Is given an endpoint and returns members" do
+    it "OpenStruct objects are created" do
+      member = SearchFacade.get_nation_member_data("fire_nation").first
+
+      expect(SearchFacade.create_member(member)).to be_an(OpenStruct)
+      expect(SearchFacade.create_member(member)).to respond_to(:id)
+      expect(SearchFacade.create_member(member)).to respond_to(:name)
+      expect(SearchFacade.create_member(member)).to respond_to(:allies)
+      expect(SearchFacade.create_member(member)).to respond_to(:enemies)
+      expect(SearchFacade.create_member(member)).to respond_to(:affiliation)
     end
   end
 end
--- spec/features/search_nations_spec.rb
+++ spec/features/search_nations_spec.rb
@@ -1,33 +1,24 @@
 require 'rails_helper'
+require 'ostruct'
 
 RSpec.describe "As a user", type: :feature do
   describe "when I visit the root path" do
     it "lets me search for members after selecting a nation" do
-      # As a user,
-      # When I visit "/"
       visit root_path
-      # And I Select "Fire Nation" from the select field
       select "Fire Nation", :from => "nation"
-      # (Note: Use the existing select field)
-      # And I click "Search For Members"
       click_on "Search For Members"
-      # Then I should be on page "/search"
+
       expect(current_path).to eq("/search")
       expect(page).to have_content("Fire Nation")
       expect(page).to have_content("97 Members")
-      # Then I should see the total number of people who live in the Fire Nation. (should be close to 100)
 
-      # And I should see a list with the detailed information for the first 25 members of the Fire Nation.
-      # And for each of the members I should see:
       within "#member-5cf5679a915ecad153ab68fd" do
         expect(page).to have_content("Name: Chan (Fire Nation admiral)")
-      # - The name of the member (and their photo, if they have one)
         expect(page).to have_content("Allies: Ozai")
-      # - The list of allies or "None"
         expect(page).to have_content("Enemies: Earth Kingdom")
-      # - The list of enemies or "None"
         expect(page).to have_content("Affiliations: Fire Nation Navy")
       end
+
       within "#member-5cf5679a915ecad153ab692a" do
         expect(page).to have_content("Name: Fire Nation train conductor")
         expect(page).to have_content("Allies: Aang")
