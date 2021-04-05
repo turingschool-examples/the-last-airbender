d3947bbfce96439aa9d52f527770846026516c13
Dominic Padula <domopadula2192@gmail.com>
2021-04-05 09:31:01 -0600

    add start of service

--- app/services/member_service.rb
+++ app/services/member_service.rb
@@ -4,7 +4,7 @@ class MemberService
     resp = Faraday.get("https://last-airbender-api.herokuapp.com/api/v1/characters?") do |req|
     req.params['affiliation'] = nation
   end
-    all_parks = JSON.parse(resp.body, symbolize_names: true)
+    all_members = JSON.parse(resp.body, symbolize_names: true)
     require "pry"; binding.pry
     MemberFacade.get_objects(all_members)
   end
