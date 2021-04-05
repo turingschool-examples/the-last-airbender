aed08e52d78d3db56653f3afa090db4ac76c682b
Katy La Tour <klatour324@gmail.com>
2021-04-05 10:20:04 -0500

    Create inital part of user story for getting to SearchController and Search index view

---
+++ spec/features/index_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+RSpec.describe 'Welcome Page' do
+  describe 'happy path' do
+    it 'visits root path and can select a nation from the select field' do
+      visit root_path
+
+    has_select?(:nation)
+
+    select 'Fire Nation', from: :nation
+
+    click_button 'Search For Members'
+
+    expect(current_path).to eq(search_path)
+    end
+  end
+end
+
+# As a user,
+# When I visit "/"
+# And I Select "Fire Nation" from the select field
+# (Note: Use the existing select field)
+# And I click "Search For Members"
+# Then I should be on page "/search"
+# Then I should see the total number of people who live in the Fire Nation. (should be close to 100)
+# And I should see a list with the detailed information for the first 25 members of the Fire Nation.
+# And for each of the members I should see:
+# - The name of the member (and their photo, if they have one)
+# - The list of allies or "None"
+# - The list of enemies or "None"
+# - Any affiliations that the member has
