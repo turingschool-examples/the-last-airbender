a89c2853242032fc3d3571711ded0a984a0beef1
Trevor Robinson <trevor.robi1254@gmail.com>
2021-04-05 10:55:34 -0600

    Complete user story

--- spec/features/search_spec.rb
+++ spec/features/search_spec.rb
@@ -16,12 +16,15 @@ RSpec.describe 'search for a members of a nation' do
   end
 
   it "displays details for first 25 members of nation" do
-    save_and_open_page
     expect(page).to have_css('.member', count: 25)
 
     within first(".member") do
       expect(page).to have_content("Name: Chan (Fire Nation admiral)")
-      
+      expect(page).to have_content("Allies: Ozai")
+      expect(page).to have_content("Enemies: Earth Kingdom")
+      expect(page).to have_content("Fire Nation Navy")
+      expect(page).to_not have_css('img')
+
     end
   end
 end
