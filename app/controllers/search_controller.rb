af4366c6913f9db1e78b36713db437bf38a99491
Trevor Robinson <trevor.robi1254@gmail.com>
2021-04-05 10:13:20 -0600

    Setup api connection in controller

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,22 @@
 class SearchController < ApplicationController
   def index
+    connection = Faraday.new('https://last-airbender-api.herokuapp.com')
+    response = connection.get("/api/v1/characters?affiliation=#{nation_query}&perPage=100&page=1")
+    @members = JSON.parse(response.body, symbolize_names: true)
     @nation = params[:nation]
+  end
+
+  private
+
+  def nation_parse
+    params[:nation].split('_')
+  end
+
+  def nation_string
+    nation_parse.join(' ').titlecase
+  end
+
+  def nation_query
+    nation_parse.join('+').titlecase
   end
 end
--- spec/features/search_spec.rb
+++ spec/features/search_spec.rb
@@ -1,17 +1,17 @@
 require 'rails_helper'
 RSpec.describe 'search for a members of a nation' do
-  it "it can select nation and search" do
+  before :each do
     visit root_path
     page.select 'Fire Nation', from: :nation
 
     click_on 'Search For Members'
-    save_and_open_page
-
+  end
+  it "it can select nation and search" do
     expect(current_path).to eq(search_path)
   end
 
-  xit "displays total number of people in nation" do
-    
+  it "displays total number of people in nation" do
+    expect(page).to have_content("Total People in Nation: 97")
 
   end
 
