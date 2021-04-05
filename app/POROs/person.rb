2c4795c3b0803d9ca76c58145805f390448df17e
Jordan Beck <jordanfbeck@gmail.com>
2021-04-05 10:57:20 -0600

    Add pagination for up to 25 people per page

--- app/POROs/person.rb
+++ app/POROs/person.rb
@@ -1,13 +1,13 @@
-class Person
-
-  attr_reader :_id, :name, :photoUrl, :allies, :enemies, :affiliation
-
-  def initialize(data)
-    @id = data[:_id]
-    @name = data[:name].capitalize
-    @photoUrl = data[:photoUrl]
-    @allies = data[:allies]
-    @enemies = data[:enemies]
-    @affiliation = data[:affiliation]
-  end
-end
+# class Person
+#
+#   attr_reader :_id, :name, :photoUrl, :allies, :enemies, :affiliation
+#
+#   def initialize(data)
+#     @id = data[:_id]
+#     @name = data[:name].capitalize
+#     @photoUrl = data[:photoUrl]
+#     @allies = data[:allies]
+#     @enemies = data[:enemies]
+#     @affiliation = data[:affiliation]
+#   end
+# end
--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,9 +3,10 @@ class SearchController < ApplicationController
     nation = params[:nation]
     nation_clean = nation.gsub("_", "+")
     conn = Faraday.new("https://last-airbender-api.herokuapp.com")
-    response = conn.get("/api/v1/characters?affiliation=#{nation_clean}")
-    data = JSON.parse(response.body, symbolize_names: true)
-    @person = Person.new(data)
-    require "pry"; binding.pry
+    response = conn.get("/api/v1/characters?affiliation=#{nation_clean}&perPage=25")
+    @nation = JSON.parse(response.body, symbolize_names: true)
+require "pry"; binding.pry
+    # data = JSON.parse(response.body, symbolize_names: true)
+    # @person = Person.new(data)
   end
 end
