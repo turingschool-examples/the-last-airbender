609bf1fe2bd4d8791daeb63f01efeced319ca53c
Alexa Morales Smyth <alexamsmyth@gmail.com>
2021-04-05 13:00:53 -0400

    Refactor get data to service object

--- app/services/search_service.rb
+++ app/services/search_service.rb
@@ -1,3 +1,14 @@
-class SearchService
+require 'ostruct'
 
-end 
+class SearchService
+  def self.get_nation_member_data(nation)
+    search_name = nation_name(nation)
+    url = "https://last-airbender-api.herokuapp.com/api/v1/characters?affiliation=#{search_name[0]}+#{search_name[1]}&perPage=200&page=1"
+    response = Faraday.get(url)
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def self.nation_name(nation)
+    nation.titleize.split
+  end
+end
--- spec/services/search_service_spec.rb
+++ spec/services/search_service_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe SearchService do
     it "visits an api endpoint and returns the member json data" do
       expect(SearchService.get_nation_member_data("fire_nation").class).to eq(Array)
       expect(SearchService.get_nation_member_data("fire_nation").count).to eq(97)
-      expect(SearchService.get_nation_member_data("fire_nation").first.class).to eq(OpenStruct)
+      expect(SearchService.get_nation_member_data("fire_nation").first.class).to eq(Hash)
     end
   end
 end
