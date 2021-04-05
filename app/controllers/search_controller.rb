73e1580885cd75e3036d0feda008c58c29e08bb9
Harrison Blake <harrison.r.blake.gmail.com>
2021-04-05 09:47:16 -0600

    Get correct amount of data from api back

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,10 +1,10 @@
 class SearchController < ApplicationController
   def index
-    nation = params[:nation].split('_')
-    response = Faraday.get("https://last-airbender-api.herokuapp.com/api/v1/characters?affiliation=#{params[:nation]}")
-    #   r.params[affiliation] = params[:nation]
-    # end
+    nation = params[:nation].gsub('_', ' ')
+    response = Faraday.get("https://last-airbender-api.herokuapp.com/api/v1/characters") do |r|
+      r.params[:affiliation] = nation
+      r.params[:perPage] = 200
+    end
     parsed = JSON.parse(response.body, symbolize_names: true)
-    binding.pry
   end
 end
