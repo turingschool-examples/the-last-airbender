913ad137e6c46344b389b1d2e0225a9d6b855468
Alexa Morales Smyth <alexamsmyth@gmail.com>
2021-04-05 12:54:00 -0400

    Refactor code to interact with openstruct object rather than hash

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -6,21 +6,4 @@ class SearchController < ApplicationController
     @nation_members = SearchFacade.get_nation_member_data(params[:nation], 25)
     @total_members = SearchFacade.get_nation_member_data(params[:nation]).count
   end
-
-  private
-
-  def create_members
-    get_nation_member_data.each do |member|
-      OpenStruct.new({
-        id: member[:_id],
-        name: member[:name],
-        allies: member[:allies],
-        enemies: member[:enemies],
-        affiliation: member[:affiliation] })
-    end
-  end
-
-  # def nation_name
-  #   params[:nation].titleize.split
-  # end
 end
--- app/facades/search_facade.rb
+++ app/facades/search_facade.rb
@@ -1,3 +1,5 @@
+require 'ostruct'
+
 class SearchFacade
 
   def self.get_nation_member_data(nation, result_count = 2000)
@@ -5,10 +7,22 @@ class SearchFacade
     url = "https://last-airbender-api.herokuapp.com/api/v1/characters?affiliation=#{search_name[0]}+#{search_name[1]}&perPage=200&page=1"
     response = Faraday.get(url)
     members = JSON.parse(response.body, symbolize_names: true)
-    members.first(result_count)
+    o_members = members.first(result_count).map do |member|
+      create_member(member)
+    end
+    o_members
   end
 
   def self.nation_name(nation)
     nation.titleize.split
+  end
+
+  def self.create_member(member)
+      OpenStruct.new({
+        id: member[:_id],
+        name: member[:name],
+        allies: member[:allies],
+        enemies: member[:enemies],
+        affiliation: member[:affiliation] })
   end
 end
