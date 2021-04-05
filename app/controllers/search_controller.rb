29ecd849bb913c2cbe92b569c50602fab33cced0
Harrison Blake <harrison.r.blake.gmail.com>
2021-04-05 11:00:00 -0600

    Change to service tests and use of service in controller

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,11 +1,12 @@
 class SearchController < ApplicationController
   def index
     nation = params[:nation].gsub('_', ' ')
-    response = Faraday.get("https://last-airbender-api.herokuapp.com/api/v1/characters") do |r|
-      r.params[:affiliation] = nation
-      r.params[:perPage] = 200
-    end
-    @parsed = JSON.parse(response.body, symbolize_names: true)
+    # response = Faraday.get("https://last-airbender-api.herokuapp.com/api/v1/characters") do |r|
+    #   r.params[:affiliation] = nation
+    #   r.params[:perPage] = 200
+    # end
+    # @parsed = JSON.parse(response.body, symbolize_names: true)
+    @parsed = BenderService.all_people_from_nation(nation)
     @people = []
 
     @parsed[0..24].each do |person|
--- spec/service/bender_spec.rb
+++ spec/service/bender_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe BenderService do
   describe "happy path" do
-    it 'tests the structure of our api data for top 25 people' do
+    it 'tests the structure of our api data for people' do
       data = BenderService.all_people_from_nation('Fire Nation')
 
       expect(data).to be_a(Array)
