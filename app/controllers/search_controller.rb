25789e2231a9a9ac6904397f9db46be6a5afac11
Dominic Padula <domopadula2192@gmail.com>
2021-04-05 11:00:53 -0600

    added the view

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,6 +3,5 @@ class SearchController < ApplicationController
   def index
     @members = MemberFacade.find_members(params[:nation])
     @full_count = MemberFacade.all_members(params[:nation])
-    require "pry"; binding.pry
   end
 end
