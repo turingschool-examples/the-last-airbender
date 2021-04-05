9a03f425051574907fb28176ecad75252cb635ca
Arika Turing <ablenker@gmail.com>
2021-04-05 10:19:15 -0600

    Fix hard code in controller

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,13 +1,11 @@
 class SearchController < ApplicationController
   def index
-    # nation = params[:nation]
-    conn = Faraday.get("https://last-airbender-api.herokuapp.com/api/v1/characters?affiliation=fire+nation")
+    nation = params[:nation].split("+")
 
-    # conn = Faraday.new(url: "https://last-airbender-api.herokuapp.com")
+    conn = Faraday.new(url: "https://last-airbender-api.herokuapp.com")
 
-    # response = conn.get("/api/v1/characters?affiliation=#{nation}")
-    json = JSON.parse(conn.body, symbolize_names: true)
+    response = conn.get("/api/v1/characters?affiliation=#{nation}")
+    json = JSON.parse(response.body, symbolize_names: true)
     @nations = json
-    # require "pry";binding.pry
   end
 end
