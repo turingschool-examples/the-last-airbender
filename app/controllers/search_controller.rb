751b220f53921c03df7db6793979c602b7421b46
Alexa Morales Smyth <alexamsmyth@gmail.com>
2021-04-05 11:42:19 -0400

    Create nation members variable and view section with attributes of each member

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,11 @@
 class SearchController < ApplicationController
   def index
     @nation = params[:nation].titleize
+    @nation_members = get_nation_member_data
+  end
+
+  private
+
+  def get_nation_member_data
   end
 end
