8dbe1471ed9e4b8e77e77fee51f323253420a7f3
Alexa Morales Smyth <alexamsmyth@gmail.com>
2021-04-05 12:17:20 -0400

    Add test for member information for first and twenty fifth

--- spec/features/search_nations_spec.rb
+++ spec/features/search_nations_spec.rb
@@ -14,20 +14,26 @@ RSpec.describe "As a user", type: :feature do
       # Then I should be on page "/search"
       expect(current_path).to eq("/search")
       expect(page).to have_content("Fire Nation")
+      expect(page).to have_content("97 Members")
       # Then I should see the total number of people who live in the Fire Nation. (should be close to 100)
 
       # And I should see a list with the detailed information for the first 25 members of the Fire Nation.
       # And for each of the members I should see:
-      within "#member-#{memeber.id}"
-        expect(page).to have_content("Name")
+      within "#member-5cf5679a915ecad153ab68fd" do
+        expect(page).to have_content("Name: Chan (Fire Nation admiral)")
       # - The name of the member (and their photo, if they have one)
-      expect(page).to have_content("Alies")
+        expect(page).to have_content("Allies: Ozai")
       # - The list of allies or "None"
-        expect(page).to have_content("Enemies")
+        expect(page).to have_content("Enemies: Earth Kingdom")
       # - The list of enemies or "None"
-      expect(page).to have_content("Affiliations")
-      # - Any affiliations that the member has
-
+        expect(page).to have_content("Affiliations: Fire Nation Navy")
+      end
+      within "#member-5cf5679a915ecad153ab692a" do
+        expect(page).to have_content("Name: Fire Nation train conductor")
+        expect(page).to have_content("Allies: Aang")
+        expect(page).to have_content("Enemies: Combustion Man")
+        expect(page).to have_content("Affiliations: Fire Nation Fire Nation train")
+      end
     end
   end
 end
