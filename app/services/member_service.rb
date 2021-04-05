8412c5e3a091805ea1e3c0c2a3ca785217b098a5
Dominic Padula <domopadula2192@gmail.com>
2021-04-05 11:22:02 -0600

    refactor

--- app/services/member_service.rb
+++ app/services/member_service.rb
@@ -9,23 +9,10 @@ class MemberService
   end
 
   def self.get_length(nation)
-    @full_count = []
-      page_num = 1
-    more_members = next_page(nation, page_num)
-    @full_count << more_members
-    until more_members.length == 0
-      page_num += 1
-      more_members = next_page(nation, page_num)
-      @full_count << more_members
-    end
-    @full_count.flatten.length
-  end
-
-  def self.next_page(nation, page_num)
     resp = Faraday.get("https://last-airbender-api.herokuapp.com/api/v1/characters?affiliation=#{nation.gsub('_', '+')}") do |req|
-      req.params['perPage'] = 25
-      req.params['page'] = page_num
+      req.params['perPage'] = 1000
     end
     all_members = JSON.parse(resp.body, symbolize_names: true)
+    all_members.length
   end
-end
+end 
