816522dab1a582843fe4be55e1dc3a49fb7e18bd
Alexa Morales Smyth <alexamsmyth@gmail.com>
2021-04-05 11:52:37 -0400

    Add endpoint data retreival for members. Utilized pagination to avoid having to visit mutliple pages for members

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -7,5 +7,12 @@ class SearchController < ApplicationController
   private
 
   def get_nation_member_data
+    url = "https://last-airbender-api.herokuapp.com/api/v1/characters?affiliation=#{nation_name[0]}+#{nation_name[1]}&perPage=200&page=1"
+    response = Faraday.get(url)
+    members = JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def nation_name
+    params[:nation].titleize.split
   end
 end
