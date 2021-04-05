b3bb3d5fbe6c352d6838636fd168d869eec62541
Jacob Arellano <jmm.arellano@gmail.com>
2021-04-05 10:40:20 -0600

    Hard code test assertions

--- spec/features/welcome/index_spec.rb
+++ spec/features/welcome/index_spec.rb
@@ -23,22 +23,35 @@ describe "As a user" do
 
       expect(page).to have_content("Nation Population: 111")
 
-      # members = SearchController.call_to_api_for_info
-
       within ".members" do
-        # expect(page).to have_content("#{members.first.name}")
-        # expect(page).to have_content("#{members.first.photo}")
-        # expect(page).to have_content("#{members.first.allies}")
-        # expect(page).to have_content("#{members.first.enemies}")
-        # expect(page).to have_content("#{members.first.affiliations}")
-        #
-        # expect(page).to have_content("#{members.last.name}")
-        # expect(page).to have_content("#{members.last.photo}")
-        # expect(page).to have_content("#{members.last.allies}")
-        # expect(page).to have_content("#{members.last.enemies}")
-        # expect(page).to have_content("#{members.last.affiliations}")
         expect(page.all('li', count: 25))
+
+        expect(page).to have_content("Name: Bolin")
+        expect(page).to have_content('Allies: ["Asami Sato"] ')
+        expect(page).to have_content('Enemies: ["Amon"] ')
+        expect(page).to have_content("Fire Ferrets (formerly) Kuvira's army (deserted) Team Avatar Triple Threat Triad (formerly) Republic City Police")
+        # expect(page).to have_xpath("https://vignette.wikia.nocookie.net/avatar/images/d/d4/Bolin.png/revision/latest?cb=20150406232314")
+
+        expect(page).to have_content("Name: Fire Nation airship captain")
+        expect(page).to have_content('Allies: ["Qin Lee"]  ')
+        expect(page).to have_content('Enemies: ["Sokka"]  ')
+        expect(page).to have_content("Fire Nation")
+
       end
     end
   end
 end
+
+
+
+# expect(page).to have_content("#{members.first.name}")
+# expect(page).to have_content("#{members.first.photo}")
+# expect(page).to have_content("#{members.first.photo}")
+# expect(page).to have_content("#{members.first.enemies}")
+# expect(page).to have_content("#{members.first.affiliations}")
+#
+# expect(page).to have_content("#{members.last.name}")
+# expect(page).to have_content("#{members.last.photo}")
+# expect(page).to have_content("#{members.last.allies}")
+# expect(page).to have_content("#{members.last.enemies}")
+# expect(page).to have_content("#{members.last.affiliations}")
