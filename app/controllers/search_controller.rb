f3445dce865ab2f370b401af054f3eb2c0b41901
Alexa Morales Smyth <alexamsmyth@gmail.com>
2021-04-05 12:22:16 -0400

    Add photo display to view

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,10 @@
+require 'ostruct'
+
 class SearchController < ApplicationController
   def index
     @nation = params[:nation].titleize
-    @nation_members = get_nation_member_data
+    @nation_members = get_nation_member_data.first(25)
+    @total_members = get_nation_member_data.count
   end
 
   private
@@ -9,7 +12,18 @@ class SearchController < ApplicationController
   def get_nation_member_data
     url = "https://last-airbender-api.herokuapp.com/api/v1/characters?affiliation=#{nation_name[0]}+#{nation_name[1]}&perPage=200&page=1"
     response = Faraday.get(url)
-    members = JSON.parse(response.body, symbolize_names: true)
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def create_members
+    get_nation_member_data.each do |member|
+      OpenStruct.new({
+        id: member[:_id],
+        name: member[:name],
+        allies: member[:allies],
+        enemies: member[:enemies],
+        affiliation: member[:affiliation] })
+    end
   end
 
   def nation_name
